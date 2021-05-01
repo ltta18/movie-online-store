ef12382192f9a8c2cce4bfe48121852c9a3f50b2
ltta18 <thuylinh7720@gmail.com>
2021-05-01 23:27:21 +0700

    Add get film api

--- src/redux/actions/film.js
+++ src/redux/actions/film.js
@@ -2,8 +2,8 @@ import FilmTypes from "../types/FilmTypes";
 import { DOMAIN, API_KEY } from "../../../config.json";
 import axios from "axios";
 
-const movieUrl = (route) =>
-  `${DOMAIN}/movie/${route}?api_key=${API_KEY}&language=en-US&page=1`;
+const movieUrl = (route, id = null) =>
+  `${DOMAIN}/movie/${route}${id && `/${id}`}?api_key=${API_KEY}&language=en-US`;
 
 export function fetchNowPlaying() {
   return {
@@ -17,6 +17,38 @@ export function fetchPopular() {
   return {
     types: FilmTypes.FETCH_POPULAR,
     callAPI: () => postApi(movieUrl("popular")),
+    payload: {},
+  };
+}
+
+export function fetchTopRated() {
+  return {
+    types: FilmTypes.FETCH_TOP_RATED,
+    callAPI: () => postApi(movieUrl("top_rated")),
+    payload: {},
+  };
+}
+
+export function fetchRecommendation(id) {
+  return {
+    types: FilmTypes.FETCH_POPULAR,
+    callAPI: () => postApi(movieUrl("recommendations", id)),
+    payload: {},
+  };
+}
+
+export function fetchImage(id) {
+  return {
+    types: FilmTypes.FETCH_IMAGE,
+    callAPI: () => postApi(movieUrl("images", id)),
+    payload: {},
+  };
+}
+
+export function fetchDetail(id) {
+  return {
+    types: FilmTypes.FETCH_DETAIL,
+    callAPI: () => postApi(movieUrl(id)),
     payload: {},
   };
 }
--- src/redux/reducers/film.js
+++ src/redux/reducers/film.js
@@ -19,7 +19,47 @@ function fetchPopular(state = {}, action) {
   }
 }
 
+function fetchTopRated(state = {}, action) {
+  switch (action.type) {
+    case FilmTypes.FETCH_TOP_RATED_SUCCESS:
+      return { ...action.data.results };
+    default:
+      return state;
+  }
+}
+
+function fetchRecommendation(state = {}, action) {
+  switch (action.type) {
+    case FilmTypes.FETCH_RECOMMENDATION:
+      return { ...action.data.results };
+    default:
+      return state;
+  }
+}
+
+function fetchImage(state = {}, action) {
+  switch (action.type) {
+    case FilmTypes.FETCH_IMAGE:
+      return { ...action.data.results };
+    default:
+      return state;
+  }
+}
+
+function fetchDetail(state = {}, action) {
+  switch (action.type) {
+    case FilmTypes.FETCH_DETAIL:
+      return { ...action.data.results };
+    default:
+      return state;
+  }
+}
+
 export default combineReducers({
   fetchNowPlaying,
   fetchPopular,
+  fetchTopRated,
+  fetchRecommendation,
+  fetchImage,
+  fetchDetail,
 });
